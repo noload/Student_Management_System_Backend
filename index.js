3771b944d2d3b181be71c50082529bfa3c6016ec
noload <vaibhavkamble023@gmail.com>
2024-02-13 21:32:08 +0530

    added dummy route

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const { PORT } = require("./config/serverConfig");
 
 const app = express();
 app.use(express.json());
+
 const studentRoute = require("./routes/Student");
 const subjectRoute = require("./routes/Subject");
 const gradeRoute = require("./routes/Grade");
 const reportRoute = require("./routes/ReportCard");
 
+app.get("/", (req, res) => {
+  res.send("this is first docker project");
+});
 app.use("/api/student", studentRoute);
 app.use("/api/subject", subjectRoute);
 app.use("/api/grade", gradeRoute);
